587d27c1dd263068f2b77dc4028e0bca701f3526
Vikash Mishra <131462517+vikashmishra1234@users.noreply.github.com>
2023-11-30 17:51:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import { ToastContainer } from 'react-toastify';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter,Link,Routes,Route } from 'react-router-dom';
+import Home from './Home';
+import Project from './components/project/Project';
+import Contact from './components/contact/Contact';
+
+function App() {
+  return (
+    <>
+    
+   <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/project' element={<Project/>}/>
+          <Route exact path='/contact' element={<Contact/>}/>
+
+         
+        </Routes>
+   </BrowserRouter>
+   <ToastContainer />
+
+    </>
+  );
+}
+
+export default App;
